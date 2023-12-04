08dd87c688fc3200502689110df6b83c9b142cff
caronavamon <caronavamon25@gmail.com>
2023-12-04 11:19:55 -0600

    Correcciones gráficos platino

--- Platino.R
+++ Platino.R
@@ -1,30 +1,47 @@
 library(readxl)
 library(ggplot2)
+library(lubridate)
 
-Futuros_platino<- read.csv("Datos históricos Futuros platino.csv",  sep = ",")
-Spot_platino <- read.csv("Datos históricos XPT_USD.csv",  sep = ",")
-
+Futuros_platino <- read.csv("Datos históricos Futuros platino.csv",  sep = ",")
 Futuros_platino[2:5] <- as.data.frame(lapply(Futuros_platino[2:5], function(x) as.numeric(gsub(",", "\\.", gsub("\\.", "", x)))))
-Spot_platino[2:5] <- as.data.frame(lapply(Spot_platino[2:5], function(x) as.numeric(gsub(",", "\\.", gsub("\\.", "", x)))))
-
-
 Futuros_platino$X..var. <- gsub(as.array("%") , "", Futuros_platino$X..var.)
 Futuros_platino$X..var. <- as.numeric(gsub(as.array(",") , ".", Futuros_platino$X..var.))/100
+Futuros_platino$Fecha <- as.Date(Futuros_platino$Fecha, format = "%d.%m.%Y")
+
+Spot_platino <- read.csv("Datos históricos XPT_USD.csv",  sep = ",")
+Spot_platino[2:5] <- as.data.frame(lapply(Spot_platino[2:5], function(x) as.numeric(gsub(",", "\\.", gsub("\\.", "", x)))))
 Spot_platino$X..var. <- gsub(as.array("%") , "", Spot_platino$X..var.)
 Spot_platino$X..var. <- as.numeric(gsub(as.array(",") , ".", Spot_platino$X..var.))/100
-
+Spot_platino$Fecha <- as.Date(Spot_platino$Fecha, format = "%d.%m.%Y")
 
 #Proyección mensual del precio spot del 1-01-23 a 2 años.
 
 #Tasa libre de riesgo
 
-r_spot <- mean(Spot_platino$X..var.[13:96])
+Faltantes_platino <- as.Date(c("2024-01-01", "2024-02-01", "2024-03-01", "2024-04-01", "2024-05-01", "2024-06-01", "2024-07-01", 
+                               "2024-08-01", "2024-09-01", "2024-10-01", "2024-11-01", "2024-12-01"))
+Fecha <- c(rev(Spot_platino$Fecha[1:12]), Faltantes_platino)
+Resultados_Spot_platino <- data.frame(Fecha = Fecha, Spot_platino_proyectados = rep(0, 24))
 
+r_spot <- mean(Spot_platino$X..var.[13:96])
 Spot_platino_proyectados <- c()
 
 for(i in 1:24){
   Spot_platino_proyectados[i] <- Spot_platino$Apertura[12]*exp(r_spot*(i/12))
 }
+
+Resultados_Spot_platino$Spot_platino_proyectados <- Spot_platino_proyectados
+
+#Comparación de los precios spot proyectados y los reales en dos años
+Comparación_spot_platino <- ggplot() +
+  geom_line(Resultados_Spot_platino, mapping=aes(x = Fecha, y = Spot_platino_proyectados, color = "Predicción"), linetype = "solid", linewidth = 1) +
+  geom_line(rev(Spot_platino[1:24,]), mapping=aes(x = Fecha, y = Apertura, color = "Real"), linetype = "solid", linewidth = 1) +
+  scale_color_manual(values = c("Predicción" = "darkblue", "Real" = "maroon")) +
+  labs(title = "", x = "Tiempo", y = "Precio Spot", color = "") +
+  theme_minimal()+
+  scale_y_continuous(labels = function(x) format(x, big.mark = " ", decimal.mark = ",", scientific = FALSE))
+
+print(Comparación_spot_platino)
 
 Spot_platino_proyeccion = ggplot() + 
   geom_line(aes(x = 1:24, y = Spot_platino_proyectados, color = "Línea de Proyección"), linetype = "solid", size = 1) + 
@@ -32,7 +49,7 @@ Spot_platino_proyeccion = ggplot() +
   cowplot::theme_cowplot() + theme_minimal() +
   scale_color_manual(values = "blue",guide = FALSE)+ 
   scale_y_continuous(labels = function(x) format(x, big.mark = " ", decimal.mark = ",", scientific = FALSE))
-  
+
 
 print(Spot_platino_proyeccion)
 
@@ -48,19 +65,9 @@ Comparación_spot_platino1 = ggplot() +
 print(Comparación_spot_platino1)
 
 
-#Comparación de los precios spot proyectados y los reales en dos años
-Comparación_spot_platino = ggplot() + 
-  geom_line(aes(x = 13:36, y = Spot_platino_proyectados, color = "Predicción"), linetype = "solid", linewidth = 1) +
-  geom_line(aes(x = 1:24, y = rev(Spot_platino$Apertura[1:24]) , color = "Real"), linetype = "solid", linewidth = 1) +
-  scale_color_manual(values = c("Predicción" = "darkblue", "Real" = "maroon")) +
-  labs(title = "", x = "Tiempo", y = "Precio Spot", color = "") +
-  cowplot::theme_cowplot() + theme_minimal()+ 
-  scale_y_continuous(labels = function(x) format(x, big.mark = " ", decimal.mark = ",", scientific = FALSE))
-
-print(Comparación_spot_platino)
-
 #Proyección mensual de futuros a 2 años
 
+Resultados_futuros_platino <- data.frame(Fecha = Fecha, Futuros_platino_proyectados = rep(0, 24))
 Futuros_platino_proyectados<- c()
 
 #Proyecciones 2023 y 2024
@@ -71,8 +78,11 @@ for(i in 2:24){
   Futuros_platino_proyectados[i] <- Spot_platino_proyectados[i-1]*exp(r_spot*(1/12))
 }
 
+Resultados_futuros_platino$Futuros_platino_proyectados <- Futuros_platino_proyectados
+
+
 Futuros_platino_proyeccion = ggplot() + 
-  geom_line(aes(x = 1:24, y = Futuros_platino_proyectados[1:24], color = "Línea de Proyección"), linetype = "solid", size = 1) + 
+  geom_line(Resultados_futuros_platino, mapping=aes(x = Fecha, y = Futuros_platino_proyectados, color = "Predicción"), linetype = "solid", linewidth = 1) + 
   labs(title = "", x = "Tiempo", y = "Precio Futuros", color = "") +
   cowplot::theme_cowplot()+theme_minimal() +
   scale_color_manual(values = "blue",guide = FALSE)+ 
@@ -95,30 +105,35 @@ print(Comparación_futuros_platino)
 
 #Comparación futuros 2023
 
+Resultados_futuros_platino_2023 <- data.frame(Fecha = rev(Spot_platino$Fecha[1:12]), Futuros_platino_2023 = rep(0, 24))
 Futuros_platino_2023 <- c()
 for(i in 1:12 ){
   r <- mean(Spot_platino$X..var.[(13-i):(25-i)])
   Futuros_platino_2023[i] <- Spot_platino$Apertura[13-i]*exp(r*(1/12))
 }
+Resultados_futuros_platino_2023$Futuros_platino_2023 <- Futuros_platino_2023
+
 
 #Comparación de los precios futuros proyectados y los reales para el 2023
-Comparación_futuros_platino2 = ggplot() + 
-  geom_line(aes(x = 1:12, y = Futuros_platino_2023[1:12] , color = "Predicción"), linetype = "solid", linewidth = 1) +
-  geom_line(aes(x = 1:12, y = rev(Futuros_platino$Apertura[1:12]) , color = "Real"), linetype = "solid", linewidth = 1) +
+
+Comparación_futuros_platino2 <- ggplot() +
+  geom_line(Resultados_futuros_platino_2023, mapping=aes(x = Fecha, y = Futuros_platino_2023, color = "Predicción"), linetype = "solid", linewidth = 1) +
+  geom_line(rev(Futuros_platino[1:12,]), mapping=aes(x = Fecha, y = Apertura, color = "Real"), linetype = "solid", linewidth = 1) +
   scale_color_manual(values = c("Predicción" = "darkblue", "Real" = "maroon")) +
   labs(title = "", x = "Tiempo", y = "Precio Futuros", color = "") +
-  cowplot::theme_cowplot() + theme_minimal()+ 
+  theme_minimal()+
   scale_y_continuous(labels = function(x) format(x, big.mark = " ", decimal.mark = ",", scientific = FALSE))
 
 print(Comparación_futuros_platino2)
+
 
 #Análisis descriptivo de los datos
 
 #Comparación futuros y spots históricos
 
 Comparación_futuros_spots = ggplot() + 
-  geom_line(aes(x = 13:96, y = rev(Spot_platino$Apertura[13:96]), color = "Spots"), linetype = "solid", linewidth = 1) +
-  geom_line(aes(x = 13:96, y = rev(Futuros_platino$Apertura[13:96]) , color = "Futuros"), linetype = "solid", linewidth = 1) +
+  geom_line(rev(Spot_platino[13:96,]),mapping = aes(x = Fecha, y = Apertura, color = "Spots"), linetype = "solid", linewidth = 1) +
+  geom_line(rev(Futuros_platino[13:96,]), mapping = aes(x = Fecha, y = Apertura , color = "Futuros"), linetype = "solid", linewidth = 1) +
   scale_color_manual(values = c("Spots" = "darkblue", "Futuros" = "maroon")) +
   labs(title = "", x = "Tiempo", y = "Precio", color = "") +
   cowplot::theme_cowplot() + theme_minimal()+ 
